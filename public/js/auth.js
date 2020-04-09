32130c10d073f5e9b181cd38efad4b099a50fef7
shimphillip <shimphillip@gmail.com>
2020-04-09 01:52:25 -0500

    register form

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,10 +1,49 @@
 const authSwitchLinks = document.querySelectorAll('.switch');
+const errorContainers = document.querySelectorAll('.error-container');
 const authModals = document.querySelectorAll('.auth-modal');
 
+const registerForm = document.querySelector('.registerForm');
+const registerFormErrorMessage = document.querySelector(
+  '.registerForm .error-container'
+);
+
+// toggle visibility of login and register forms
 authSwitchLinks.forEach((link) => {
   link.addEventListener('click', () => {
     authModals.forEach((modal) => {
       modal.classList.toggle('active');
     });
   });
+});
+
+// close error messages clicking on 'x' buttons
+errorContainers.forEach((errorMessage) => {
+  errorMessage.addEventListener('click', (e) => {
+    console.log(e.target);
+    console.log(e.target.classList);
+    if (e.target.classList[0] === 'close') {
+      errorMessage.classList.remove('active');
+    }
+  });
+});
+
+// New user registration on the front-end
+registerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = registerForm.email.value;
+  const password = registerForm.password.value;
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log('registered', user);
+      registerForm.reset();
+    })
+    .catch((error) => {
+      registerForm.querySelector('.error-message').textContent = error.message;
+      registerFormErrorMessage.classList.add('active');
+      registerForm.reset();
+    });
 });

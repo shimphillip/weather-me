228f1fedfc86f735f4e42c2d3e4ff9860f548c7b
shimphillip <shimphillip@gmail.com>
2020-04-10 00:26:26 -0500

    basic twilio function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
+require('dotenv').config();
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+exports.sendMessagePlease = functions.https.onRequest((request, response) => {
+  const client = require('twilio')(
+    process.env.ACCOUNT_SID,
+    process.env.AUTH_TOKEN
+  );
 
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+  client.messages
+    .create({
+      body: 'All in the game, yo',
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: '+15125956354',
+    })
+    .then((message) => {
+      console.log(message.sid);
+      return response.send(message.sid);
+    })
+    .catch((error) => {
+      console.log(error);
+      return response.send(error);
+    });
+});
